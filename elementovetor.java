4e5ec6de0a467ace2a11e7897950e266bf728e53
ldss2002 <32168991+ldss2002@users.noreply.github.com>
2018-04-05 16:11:19 +0100

    Add files via upload

---
+++ elementovetor.java
@@ -0,0 +1,44 @@
+/*
+ * Leia um vetor de 20 posições
+ * leia um valor X qualquer
+ * Faça uma busca de X no vetor
+ * informe a posição do vetor que X foi encontrado
+ * ou diga que não foi encontrado
+ */
+
+package elementovetor;
+import java.util.Scanner;
+
+public class elementovetor {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        
+        // declaraçao do vetor
+        int[] vetor = new int[10];
+        
+        // declaracao de variavel
+        int num;
+        
+        // entrada de dados -> vetor
+        for(int i = 0; i < vetor.length; i++){
+            System.out.println("Digite um valor para a posição " + i);
+            vetor[i] = in.nextInt();
+        }
+        
+        // entrada de dados -> variável
+        System.out.println("Digite um valor para procurar no vetor");
+        num = in.nextInt();
+        
+        // leitura do vetor
+        for(int i = 0; i < vetor.length; i++){
+            
+            // se existir no vetor o número digitado
+            if(vetor[i] == num){
+                System.out.println("O índice do número digitado é " + i);
+            // se não existir...
+            } else if(i == vetor.length-1){
+                System.out.println("Número não encontrado no vetor");
+            }
+        }
+    }
+}
